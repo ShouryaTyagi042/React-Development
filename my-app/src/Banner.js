18818d62889ba7ff090b32c693737901c35f79e6
Shourya Tyagi <shouryatyagi042@gmail.com>
2023-01-08 23:20:48 +0530

    Updated Team.js

--- my-app/src/Banner.js
+++ my-app/src/Banner.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./Banner.css";
 
 function Banner() {
   const [count, setCount] = useState(0);
@@ -8,7 +9,7 @@ function Banner() {
   });
 
   return (
-    <div>
+    <div className="banner">
       <p>You clicked this button {count} times.</p>
       <button onClick={() => setCount(count + 1)}>click me</button>
     </div>
---
+++ my-app/src/Team.js
@@ -0,0 +1,7 @@
+import React, { useEffect, useState } from "react";
+
+function Team() {
+  return <p>hi</p>;
+}
+
+export default Team;
